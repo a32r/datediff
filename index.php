64b05adef3b82b8b75bcbe0b26c4b5f9ae304be6
Admin <site@mail.com>
2023-12-16 21:47:49 +0300

    Started work on yandex test exercise

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Тестовое задание Яндекс</title>
+</head>
+<body>
+<?php
+
+echo "Test entity";
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
