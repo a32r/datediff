898bd02abe7f4a39446104acea01bd3188a15dfc
Admin <site@mail.com>
2023-12-18 23:14:25 +0300

    Added getSeconds function

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 </head>
 <body>
 <?php
+function getSeconds($str) {
+	global $month;
+	$input = array_map('intval', explode(" ",$str));
+	$year2seconds = $input[0] * 365 * 24 * 60 * 60;
+	$month2seconds = array_sum(array_slice($month, 0, $input[1] - 1)) * 24 * 60 * 60;
+	$days2seconds = $input[2] * 24 * 60 * 60;
+	$hours2seconds = $input[3] * 60 * 60;
+	$minutes2seconds = $input[4] * 60;
+	$seconds = $input[5];
+	return $year2seconds + $month2seconds + $days2seconds + $hours2seconds + $minutes2seconds + $seconds;
+}
 /* определение переменных */
 $data = "input.txt";
 /* массив месяцев года по количеству дней */
@@ -25,8 +36,7 @@ $month = [
 ];
 /* получение исходных параметров */
 $input = file($data);
-/* var_dump($input[1]); */
-echo $input[1];
+var_dump(getSeconds($input[1]));
 ?>
 </body>
 </html>
