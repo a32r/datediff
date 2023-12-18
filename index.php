4511ce211d0f28cde99d594b41908a598d2649af
Admin <site@mail.com>
2023-12-18 23:31:25 +0300

    Get final exercise value in browser

--- index.php
+++ index.php
@@ -5,18 +5,9 @@
 	<title>Тестовое задание Яндекс</title>
 </head>
 <body>
+
 <?php
-function getSeconds($str) {
-	global $month;
-	$input = array_map('intval', explode(" ",$str));
-	$year2seconds = $input[0] * 365 * 24 * 60 * 60;
-	$month2seconds = array_sum(array_slice($month, 0, $input[1] - 1)) * 24 * 60 * 60;
-	$days2seconds = $input[2] * 24 * 60 * 60;
-	$hours2seconds = $input[3] * 60 * 60;
-	$minutes2seconds = $input[4] * 60;
-	$seconds = $input[5];
-	return $year2seconds + $month2seconds + $days2seconds + $hours2seconds + $minutes2seconds + $seconds;
-}
+
 /* определение переменных */
 $data = "input.txt";
 /* массив месяцев года по количеству дней */
@@ -34,9 +25,30 @@ $month = [
 	11 => 30,
 	12 => 31,
 ];
+
+/* определение функций */
+function getSeconds($str) {
+	global $month;
+	$input = array_map('intval', explode(" ",$str));
+	$year2seconds = $input[0] * 365 * 24 * 60 * 60;
+	$month2seconds = array_sum(array_slice($month, 0, $input[1] - 1)) * 24 * 60 * 60;
+	$days2seconds = $input[2] * 24 * 60 * 60;
+	$hours2seconds = $input[3] * 60 * 60;
+	$minutes2seconds = $input[4] * 60;
+	$seconds = $input[5];
+	return $year2seconds + $month2seconds + $days2seconds + $hours2seconds + $minutes2seconds + $seconds;
+}
+
 /* получение исходных параметров */
 $input = file($data);
-var_dump(getSeconds($input[1]));
+
+$secondsDiff = getSeconds($input[1]) - getSeconds($input[0]);
+$days = intdiv($secondsDiff, 86400);
+$remainder = $secondsDiff % 86400;
+
+echo $days . " " . $remainder;
+
 ?>
+
 </body>
 </html>
